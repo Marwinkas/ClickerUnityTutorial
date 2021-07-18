fad54266a19900468d2f45fa1febeaa2446a2691
Marwinkas <87604769+Marwinkas@users.noreply.github.com>
2021-07-18 15:08:40 +0300

    Add files via upload

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class Click : MonoBehaviour
+{
+    public static int _money;
+    private int _clickcount = 1;
+    [SerializeField]private Text _moneytext;
+    public void GetClickCount(int clickcount)
+    {
+        _clickcount += clickcount;
+    }
+    void Update()
+    {
+        _moneytext.text = _money + "$";
+    }
+    public void Clicked()
+    {
+        _money += _clickcount;
+    }
+}
---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] private float[] _cost;
+    [SerializeField] private int[] _cpscount;
+    [SerializeField] private int _clickcount;
+    [SerializeField] private int _cps;
+    [SerializeField] private float _multiplier;
+
+    [SerializeField] private Text[] _costtext;
+    [SerializeField] private Text _cpstext;
+
+    [SerializeField] private GameObject _clickobject;
+    public void BuyPerClick()
+    {
+        if(Click._money >= _cost[0])
+        {
+            Click._money -= (int)_cost[0];
+            _cost[0] *= _multiplier;
+            _clickobject.GetComponent<Click>().GetClickCount(_clickcount);
+            _costtext[0].text = (int)_cost[0] + "";
+        }
+    }
+    public void BuyPerSecond(int select)
+    {
+        if(Click._money >= _cost[select])
+        {
+            Click._money -= (int)_cost[select];
+            _cost[select] *= _multiplier;
+            _cps += _cpscount[select - 1];
+            _costtext[select].text = (int)_cost[select] + "";
+        }
+    }
+    IEnumerator ClickPerSecond()
+    {
+        while (true)
+        {
+            Click._money += _cps;
+            _cpstext.text = "DPS: " + _cps;
+            yield return new WaitForSeconds(1);
+        }
+    }
+    void Start()
+    {
+        StartCoroutine(ClickPerSecond());
+    }
+}
